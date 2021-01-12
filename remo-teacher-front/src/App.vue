343bb0233fde94156bb2ebc5578628ed1c0095f4
temyrrian <artembogdan@outstaffing.no>
2021-01-12 15:34:59 +0200

    init

---
+++ remo-teacher-front/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <footer class="footer fixed-bottom mt-5">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/js/bootstrap.bundle";
+import HelloWorld from "./components/HelloWorld.vue";
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ remo-teacher-front/src/components/HelloWorld.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="hello">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">RemoTeacher</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+              >
+                Про викладача
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Біографічні дані</a></li>
+                <li><a class="dropdown-item" href="#">Досвід роботи</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Перелік технологій</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Співпраця</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+                <li><hr class="dropdown-divider" /></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"
+                >Наукові роботи</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"
+                >Освітня діяльність</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"
+                >Календар подій</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"
+                >Завдання</a
+              >
+            </li>
+          </ul>
+        </div>
+        <i class="fas fa-user"></i>
+      </div>
+    </nav>
+
+    <main role="main" class="container mt-5">
+      <!-- Content here -->
+      <div class="card mt-3">
+        <div class="card-body">
+          This is some text within a card body. This is some text within a card
+          body. This is some text within a card body. This is some text within a
+          card body. This is some text within a card body. This is some text
+          within a card body. This is some text within a card body. This is some
+          text within a card body. This is some text within a card body. This is
+          some text within a card body. This is some text within a card body.
+        </div>
+      </div>
+
+      <div class="card mt-3">
+        <div class="card-body">
+          This is some text within a card body. This is some text within a card
+          body. This is some text within a card body. This is some text within a
+          card body. This is some text within a card body. This is some text
+          within a card body. This is some text within a card body. This is some
+          text within a card body. This is some text within a card body. This is
+          some text within a card body. This is some text within a card body.
+        </div>
+      </div>
+
+      <div class="card mt-3">
+        <div class="card-body">
+          This is some text within a card body. This is some text within a card
+          body. This is some text within a card body. This is some text within a
+          card body. This is some text within a card body. This is some text
+          within a card body. This is some text within a card body. This is some
+          text within a card body. This is some text within a card body. This is
+          some text within a card body. This is some text within a card body.
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+	name: "HelloWorld",
+
+	props: {
+		msg: String,
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
